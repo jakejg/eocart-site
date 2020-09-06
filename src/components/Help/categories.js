b2f321be392ed873c40161c4639914c7aeadb227
Zeeshan Junaid <zeeshanjunaid2222@gmail.com>
2020-09-06 09:18:02 +0500

    categories are functional

--- src/components/Help/categories.js
+++ src/components/Help/categories.js
@@ -1,54 +1,63 @@
 import React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import { Container } from "react-bootstrap";
-import { Link } from "gatsby";
-const cats = [
-  {
-    id: 1,
-    title: "Getting Started",
-    image: "globe.svg",
-  },
-  {
-    id: 2,
-    title: "Account Questions",
-    image: "people.svg",
-  },
-  {
-    id: 3,
-    title: "Why Carbon Offsets",
-    image: "trees.svg",
-  },
-  {
-    id: 4,
-    title: "EcoPoints",
-    image: "echopoints.svg",
-  },
-  {
-    id: 5,
-    title: "About EcoCart",
-    image: "plane.svg",
-  },
-  {
-    id: 6,
-    title: "Privacy",
-    image: "privacy.svg",
-  },
-];
+import Globe from "../../images/globe.svg";
+import People from "../../images/people.svg";
+import Trees from "../../images/trees.svg";
+import EcoPoints from "../../images/echopoints.svg";
+import Plane from "../../images/plane.svg";
+import Privacy from "../../images/privacy.svg";
 
 const Categories = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      wordpress {
+        ecocartFaqCategories(where: { orderby: TERM_ORDER }) {
+          edges {
+            node {
+              id
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const getImages = (slug) => {
+    if (slug === "getting-started") {
+      return <img className="category_img" alt="category logo" src={Globe} />;
+    }
+    if (slug === "account-question") {
+      return <img className="category_img" alt="category logo" src={People} />;
+    }
+    if (slug === "why-carbon-offsets") {
+      return <img className="category_img" alt="category logo" src={Trees} />;
+    }
+    if (slug === "ecopoints") {
+      return (
+        <img className="category_img" alt="category logo" src={EcoPoints} />
+      );
+    }
+    if (slug === "about-ecocart") {
+      return <img className="category_img" alt="category logo" src={Plane} />;
+    }
+    if (slug === "privacy") {
+      return <img className="category_img" alt="category logo" src={Privacy} />;
+    }
+  };
   return (
     <div className="categories_section">
       <Container>
         <h2>Choose a category:</h2>
         <div className="cat_grid">
-          {cats.map((cat) => (
-            <Link to={`/category/${cat.id}`} key={cat.id}>
+          {data.wordpress.ecocartFaqCategories.edges.map((cat) => (
+            <Link to={`/category/${cat.node.slug}`} key={cat.node.id}>
               <div className="category">
-                <img
-                  className="category_img"
-                  alt="category logo"
-                  src={require(`../../images/${cat.image}`)}
-                />
-                <h3 className="category_title">{cat.title}</h3>
+                {getImages(cat.node.slug)}
+
+                <h3 className="category_title">{cat.node.name}</h3>
               </div>
             </Link>
           ))}

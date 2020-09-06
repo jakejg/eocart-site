cbc308074a3adf2542f4824e251bf5d146efbbea
Zeeshan Junaid <zeeshanjunaid2222@gmail.com>
2020-09-07 00:51:55 +0500

    form updated

--- src/components/General/contactWidget.js
+++ src/components/General/contactWidget.js
@@ -46,7 +46,7 @@ const ContactWidget = () => {
     tl.to(".contact_form", {
       duration: 0.7,
       ease: "power1.out",
-      y: 1000,
+      y: 100,
     }).to(".contact_btn", {
       duration: 0.1,
       ease: "power3.in",

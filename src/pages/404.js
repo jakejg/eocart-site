d4c0c6b7aae64e5ca7bb732d3613ff92e4387aa8
Zeeshan Junaid <zeeshanjunaid2222@gmail.com>
2020-09-22 03:14:13 +0500

    added 404 page design

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
-import React from "react"
+import { Link } from "gatsby";
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="notFound-page">
+      <h1>404</h1>
+      <h6>PAGE NOT FOUND.</h6>
+      <p>
+        You just hit a route that doesn&#39;t exist.
+        <Link to="/"> Go back to home</Link>
+      </p>
+    </div>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;

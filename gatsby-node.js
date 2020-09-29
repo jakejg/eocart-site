2a9eaefea5b8926d8c9e7cad872e71917c82963a
Zeeshan Junaid <zeeshanjunaid2222@gmail.com>
2020-09-29 23:19:57 +0500

    fixed dynamic page generator for wp

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,7 +11,7 @@ exports.createPages = async ({ graphql, actions }) => {
             name
           }
         }
-        ecocartFaqs {
+        ecocartFaqs(first: 100) {
           nodes {
             slug
             title

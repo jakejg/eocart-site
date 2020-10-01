27146299202abc125d1fcd5d3a2e9a287dff35fb
Ecocart-io <66227262+Ecocart-io@users.noreply.github.com>
2020-09-30 20:01:53 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,19 +4,23 @@ module.exports = {
     menuLinks:[
       {
          name:'Carbon Neutral Shopping | EcoCart Chrome Extension',
-         link:'/'
+         link:'/',
+         description:'EcoCart is a Chrome extension that automatically calculates and offsets the carbon footprint of your online orders. Earn rewards at over 10,000 online stores.'
       },
       {
          name:'Fighting Climate Change with EcoCart | EcoCart',
-         link:'/about'
+         link:'/about',
+         description:'EcoCart provides convenient and free solutions for e-commerce brands and online shoppers to make online orders carbon neutral.'
       },
       {
         name: 'Carbon Neutral Shipping | EcoCart for Shopify',
-        link:'/app'
+        link:'/app',
+        description:'Adding EcoCart to your Shopify store makes your brand more sustainable and offers your customers carbon neutral shipping at checkout.'
       },
       {
         name: 'Our Carbon Offset Projects | EcoCart',
-        link:'/app/our-projects/'
+        link:'/app/our-projects/',
+        description:'EcoCart donates to carbon offset projects that are certified to reduce the environmental impact of our everyday activities, such as shopping online.'
       }
    ],
     description: `A free and simple way to show your customers you care about the environment as much as they do.`,

a91b28ab38bb60a9a63db70a2605301daae5682a
zainlks <lakhani.zain786@gmail.com>
2020-10-15 21:13:06 -0400

    CTA injection

--- src/pages/index.js
+++ src/pages/index.js
@@ -198,7 +198,29 @@ const IndexPage = () => (
         </Row>
       </Container>
     </section>
-    <Cta />
+    <section className="cta">
+      <Container>
+        <Row>
+          <Col sm>
+            <h2>
+              Ready to start
+              <br />
+              shopping sustainably?
+            </h2>
+            <a
+              className="btn navbar-button"
+              size="small"
+              href="https://chrome.google.com/webstore/detail/ecocart-carbon-neutral-sh/oiafedhhdhinjnianpfeaenmchnknchi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <strong>Add to Chrome</strong> — It's Free!
+            </a>
+          </Col>
+        </Row>
+        <div id="hubspotForm2"></div>
+      </Container>
+    </section>
   </Layout>
 );
 

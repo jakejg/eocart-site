202b847dbcf7ad9b42ad8f57ea86a959ba1b8ca2
Jake <jakefrisbee@gmail.com>
2020-10-26 07:32:34 -0600

    added sad icon

--- src/pages/uninstall.js
+++ src/pages/uninstall.js
@@ -3,6 +3,8 @@ import Layout from "../components/layout";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import SEO from "../components/seo";
 import '../styles/pages/_uninstall.scss';
+import uninstallDane from '../images/uninstall-dane.png';
+import sadFaceIcon from '../images/disappointed-face.png';
 
 const Uninstall = () => {
     const [showThanks, setShowThanks] = useState(false);
@@ -37,37 +39,42 @@ const Uninstall = () => {
         <Layout>
           <SEO title="Uninstall" />
           <div className="uninstall">
-              <div className="top-space"></div>
-              <Container className="uninstall-title">
-                <h1 className="uninstall-title"> We’re sorry to see you go</h1>
-                <h3>Didn’t mean to uninstall?</h3>
-                <a
-                    className="btn uninstall-buttons"
-                    size="small"
-                    href="https://chrome.google.com/webstore/detail/ecocart-carbon-neutral-sh/oiafedhhdhinjnianpfeaenmchnknchi"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <strong>Re-Install EcoCart to Chrome</strong>
-                  </a>
-                </Container>
+                <section className="uninstall-section-1">
+                    <div className="top-space"></div>
+                    <Container className="uninstall-title">
+                        <h1 className="uninstall-title"> We’re sorry to see you go <img className="sad-icon" src={sadFaceIcon} /></h1>
+                        <h3>Didn’t mean to uninstall?</h3>
+                        <a
+                            className="btn uninstall-buttons"
+                            size="small"
+                            href="https://chrome.google.com/webstore/detail/ecocart-carbon-neutral-sh/oiafedhhdhinjnianpfeaenmchnknchi"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <strong>Re-Install EcoCart to Chrome</strong>
+                          </a>
+                      </Container>
 
-                <Container className="uninstall-container">
-                    {!showThanks &&
-                    <form className="uninstall-options-form">
-                        {options.map(option => {
-                            return <div className={selected === option.id ? "options options-selected" : "options"} id={option.id} value={option.text} onClick={handleSelect}>{option.text}</div>
-                        })}
-                        <input placeholder="Other (write in)" id={4} onClick={handleSelect} value={customInput} onChange={handleChange} className={selected === 4 ? "options u-input options-selected" : "options u-input"}/>
-                        <div className=" btn uninstall-buttons submit-button" onClick={handleSubmit}>Submit</div>
-                    </form>
-                    }
-                    {showThanks &&
-                    <div className="uninstall-thank-you">Thank you for your feedback! We’re always just 2 clicks away if you change your mind.
-                    </div>
-                    }
-                </Container>
-                
+                      <Container className="uninstall-container">
+                          {!showThanks &&
+                          <form className="uninstall-options-form">
+                              <h2>Let us know why you’re leaving so we can do better</h2>
+                              {options.map(option => {
+                                  return <div className={selected === option.id ? "options options-selected" : "options"} id={option.id} value={option.text} onClick={handleSelect}>{option.text}</div>
+                              })}
+                              <input placeholder="Other (write in)" id={4} onClick={handleSelect} value={customInput} onChange={handleChange} className={selected === 4 ? "options u-input options-selected" : "options u-input"}/>
+                              <div className=" btn uninstall-buttons submit-button" onClick={handleSubmit}>Submit</div>
+                          </form>
+                          }
+                          {showThanks &&
+                          <div className="uninstall-thank-you">Thank you for your feedback! We’re always just 2 clicks away if you change your mind.
+                          </div>
+                          }
+                      </Container>
+                </section>
+                <section className="uninstall-section-2">
+                        <img className="uninstall-dane" src={uninstallDane} />
+                </section>
             </div>
         </Layout>
     );

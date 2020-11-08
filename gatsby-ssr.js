d0f1ed081b033ba95c9ef5b5ee9aee78ddbf1b2e
Zain Lakhani <lakhani.zain786@gmail.com>
2020-11-07 23:19:11 -0500

    Allow 200 redirect proxying into site (wordpress + gtm fix)

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,1 +1,27 @@
 export {wrapRootElement} from './gatsby-browser';
+import React from 'react'
+import { withPrefix } from 'gatsby'
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+	setPostBodyComponents([
+		<script>
+    window.onload = () => {
+      window.history.replaceState({}, '', '/')
+    }
+    </script>
+	])
+}
+
+export const onPreRenderHTML = ({ getPostBodyComponents, replacePostBodyComponents }) => {
+	let postBodyComponents = getPostBodyComponents()
+	postBodyComponents.sort((a, b) => {
+		if (a.key === 'replaceURL') {
+			return 1
+		}
+		if (b.key === 'replaceURL') {
+			return -1
+		}
+		return 0
+	})
+	replacePostBodyComponents(postBodyComponents)
+}
